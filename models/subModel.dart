de31f2b1de54096e94835583aa33b9e70dfff899
youyoung00 <80229988+youyoung00@users.noreply.github.com>
2021-05-04 01:52:39 +0900

    Create subModel.dart

---
+++ models/subModel.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/foundation.dart';
+
+class SubModel{
+  String title;
+  String singer;
+  String lyrics;
+  String jacket;
+  String titles;
+
+  SubModel({
+    @required this.title,
+    @required this.singer,
+    @required this.lyrics,
+    @required this.jacket,
+    @required this.titles
+  });
+}
