5e1166291c3c8e71f6cb0dd8f5bd088a0c94f5cf
youyoung00 <80229988+youyoung00@users.noreply.github.com>
2021-08-23 15:37:17 +0900

    Update mainDataModel.dart

--- models/mainDataModel.dart
+++ models/mainDataModel.dart
@@ -1,14 +1,27 @@
 import 'package:flutter/foundation.dart';
-import 'package:sh_selfstudy_ver0417/models/subModel.dart';
+import 'package:jamesclass_musicapp_self/DataModels/SubDataModel.dart';
 
 class MainDataModel{
-  String title;
-  List<SubModel> datas;
 
+  String? category;
+  String? img;
+  List<SubDataModel>? subData;
 
   MainDataModel({
-    @required this.title,
-    @required this.datas
+    required this.category,
+    required this.img,
+    required this.subData
   });
-}
 
+  MainDataModel.init(Map<String,dynamic> e){
+    this.category = e["category"].toString();
+    this.img = e["img"].toString();
+    this.subData = List.of(e["subData"]).map<SubDataModel>((dynamic v) {
+      return SubDataModel.init(v);
+    }).toList();
+  }
+
+  // factory MainDataModel.fInit(Map<String,dynamic> e){
+  //   return new MainDataModel.init(e);
+  // }
+}
