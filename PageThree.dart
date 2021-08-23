e8b7b0a04fc5eea98e13e2b630679b24575661fa
youyoung00 <80229988+youyoung00@users.noreply.github.com>
2021-08-23 15:36:41 +0900

    Update PageThree.dart

--- PageThree.dart
+++ PageThree.dart
@@ -1,84 +1,84 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:sh_selfstudy_ver0417/models/subModel.dart';
+import 'package:flutter/foundation.dart';
+import 'package:jamesclass_musicapp_self/DataModels/MainDataModel.dart';
+import 'package:jamesclass_musicapp_self/DataModels/SubDataModel.dart';
 
-class PageThree extends StatelessWidget {
-  // int i;
-  // PageThree(this.i);
-  String title;
-  SubModel sData;
-  //List<SubModel> datas;
-  PageThree ({@required this.sData, @required this.title,});
-  // String titles;
-  // String singer;
-  // String lyrics;
-  // String jacket;
-  // String title;
-   //PageThree({@required this.titles, @required this.singer, @required this.lyrics, @required this.jacket, @required this.title});
+class ThreePage extends StatelessWidget {
+
+  SubDataModel? subData; // 어떤 타입으로 받을 것인지 매우 중요. List 안의 Map 을 바로 subData.title 처럼 입력 가능하도록  .
+  String? category;
+
+  ThreePage( { required this.category, required this.subData } );
 
   @override
   Widget build(BuildContext context) {
+    print(this.category);
+    print(this.subData);
+
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.deepPurple,
-        title: Text(this.title),
+        backgroundColor: Colors.yellow,
+        iconTheme: IconThemeData(color: Colors.brown),
         centerTitle: true,
+        title: Text(
+          this.category.toString(),
+          style: TextStyle(
+            color: Colors.brown
+          ),
+        ),
       ),
-      body: ListView(
-        children: [
-          this._musicitems(context)
-        ],
-      ),
+      body: this._detailData(context),
     );
   }
-  Widget _musicitems(BuildContext context){
-    return Container(
-      child: Column(
-        children: [
-          Container(
-            padding: EdgeInsets.only(top: 20.0, left: 10.0, right: 10.0, bottom: 5.0),
-            width: MediaQuery.of(context).size.width,
-            child: Text(
-              this.sData.titles,
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 25.0,
-              ),
-              textAlign: TextAlign.center,
-            ),
-            //color: Colors.black26,
-          ),
-          Container(
-            padding: EdgeInsets.only(top: 10.0, left: 10.0, right: 10.0, bottom: 0.0),
-            width: MediaQuery.of(context).size.width,
-            child: Text(
-              this.sData.singer,
-              textAlign: TextAlign.center,
-              style: TextStyle(fontSize: 16.0),
-            ),
-            //color: Colors.black38,
-          ),
-          Container(
-            padding: EdgeInsets.all(10.0),
-            width: MediaQuery.of(context).size.width,
-            child: Image.network(this.sData.jacket,
-              // fit: BoxFit.cover
-            ),
 
-            //color: Colors.red,
-            // Container(),
-          ),
-          Container(
-            //color: Colors.red,
-            padding: EdgeInsets.all(10.0),
-            width: MediaQuery.of(context).size.width,
-            child: Text(this.sData.lyrics,
-              //textAlign: TextAlign.left,
-              style: TextStyle(fontSize: 18.0,),
+  Widget _detailData(BuildContext context){
+
+    return SingleChildScrollView(
+      child: Container(
+        padding: EdgeInsets.all(20.0),
+        width: MediaQuery.of(context).size.width,
+        child: Column(
+          // children: List.of(this.detailData!).map<Widget>(
+          //       (e) =>
+          // ).toList()
+          children: [
+            Container(
+              child: Text(
+                this.subData!.title!,
+                style: TextStyle(
+                 fontWeight: FontWeight.bold,
+                  fontSize: 18.0
+                ),
+              ),
             ),
-            //color: Colors.blue,
-          ),
-        ],
-      ),
+            Container(
+              padding: EdgeInsets.only(top: 10.0),
+              child: Text(
+                  this.subData!.singer!
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.symmetric(vertical: 20.0),
+              height: 400,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  fit: BoxFit.cover,
+                  image: NetworkImage(
+                    this.subData!.jacket!
+                  )
+                )
+              ),
+            ),
+            Container(
+              alignment: Alignment.centerLeft,
+              child: Text(
+                this.subData!.songText!,
+              ),
+            ),
+          ],
+        )
+      )
     );
   }
 }
