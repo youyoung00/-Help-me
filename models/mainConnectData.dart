0326371ed97147852a3687591b4509f3564fd510
youyoung00 <80229988+youyoung00@users.noreply.github.com>
2021-09-05 16:52:56 +0900

    Create mainConnectData.dart

---
+++ models/mainConnectData.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/foundation.dart';
+import 'package:jamesclass_musicapp_self/DataModels/MainDataModel.dart';
+
+class MainConnectData{
+  List<MainDataModel>? data;
+  String? viewTxt;
+  MainConnectData({
+    required this.data,
+    required this.viewTxt,
+  });
+}
