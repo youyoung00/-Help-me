30af3535d643e765ce05dfe4137748c2eed77a85
youyoung00 <80229988+youyoung00@users.noreply.github.com>
2021-09-05 16:52:10 +0900

    Update subModel.dart

--- models/subModel.dart
+++ models/subModel.dart
@@ -6,6 +6,7 @@ class SubDataModel{
   String? jacket;
   String? songText;
 
+  /// 모델링 1: 클래스의 기본 생성자 사용
   SubDataModel({
     required this.title,
     required this.jacket,
@@ -13,17 +14,31 @@ class SubDataModel{
     required this.singer,
   });
 
+  /// 모델링 2: 클래스의 이름있는 생성자 사용.
   SubDataModel.init(Map<String,dynamic> v){
     this.title = v["title"].toString();
-    this.jacket = v["jacket"].toString();
-    this.songText = v["songText"].toString();
     this.singer = v["singer"].toString();
-    //return SubDataModel.init(v);
-    //return SubDataModel();
+    this.songText = v["songText"].toString();
+    this.jacket = v["jacket"].toString();
   }
 
+  /// 모델링 3: 팩토리 메서드로 인스턴스 반복 사용.
+  factory SubDataModel.fInit(Map<String,dynamic> v){
+    return SubDataModel.init(v);
+  }
+  // SubDataModel.init(Map<String,dynamic> v){
+  //   this.title = v["title"].toString();
+  //   this.jacket = v["jacket"].toString();
+  //   this.songText = v["songText"].toString();
+  //   this.singer = v["singer"].toString();
+  // }
+
+    //return SubDataModel.init(v);
+    //return SubDataModel();
+
   // factory SubDataModel.fInit(Map<String,dynamic> v){
-  //   //return new SubDataModel();
-  //   return new SubDataModel.init(v);
+  //   return SubDataModel.fInit(v);
+    //return new SubDataModel(title: , jacket: , songText: , singer: ,);
+    //return new SubDataModel.init(v);
   // }
 }
